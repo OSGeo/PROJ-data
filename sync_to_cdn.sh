29dbf9a6431ea1267e19cb64f196aa79b9a9e66a
Even Rouault <even.rouault@spatialys.com>
2019-12-17 23:19:01 +0100

    sync_to_cdn.sh: set content-type

--- sync_to_cdn.sh
+++ sync_to_cdn.sh
@@ -6,5 +6,5 @@ for i in `ls -d */`; do
     echo "Synchronizing $i ..."
     aws s3 sync $i "$TARGET" --exclude ".github/*" --profile projcdn
 done
-aws s3 cp index.html "$TARGET" --profile projcdn
-aws s3 cp README.DATUMGRID "$TARGET" --profile projcdn
+aws s3 cp index.html "$TARGET" --profile projcdn --content-type "text/html"
+aws s3 cp README.DATUMGRID "$TARGET" --profile projcdn --content-type "text/plain"
