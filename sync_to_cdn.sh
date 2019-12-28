ffa23a5e5e9327a34f2dd3b39b2de92b386ebf28
Even Rouault <even.rouault@spatialys.com>
2019-12-28 17:41:30 +0100

    index.html: display values using geotiff.js

--- sync_to_cdn.sh
+++ sync_to_cdn.sh
@@ -6,6 +6,9 @@ for i in `ls -d *_*/`; do
     echo "Synchronizing $i ..."
     aws s3 sync $i "$TARGET" --exclude ".github/*" --profile projcdn
 done
+aws s3 cp geotiff.bundle.min.js "$TARGET" --profile projcdn
+aws s3 cp ol.js "$TARGET" --profile projcdn
+aws s3 cp ol.css "$TARGET" --profile projcdn
 aws s3 cp index.html "$TARGET" --profile projcdn --content-type "text/html"
 aws s3 cp files.geojson "$TARGET" --profile projcdn --content-type "application/geo+json"
 aws s3 cp README.DATUMGRID "$TARGET" --profile projcdn --content-type "text/plain"
