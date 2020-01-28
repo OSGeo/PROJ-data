e13a0c26b3a1bdf2435cdfd6722d5b1730ded952
Even Rouault <even.rouault@spatialys.com>
2020-01-28 16:21:27 +0100

    sync_to_cdn.sh: fix

--- sync_to_cdn.sh
+++ sync_to_cdn.sh
@@ -11,4 +11,4 @@ aws s3 cp ol.js "$TARGET" --profile projcdn
 aws s3 cp ol.css "$TARGET" --profile projcdn
 aws s3 cp index.html "$TARGET" --profile projcdn --content-type "text/html"
 aws s3 cp files.geojson "$TARGET" --profile projcdn --content-type "application/geo+json"
-aws s3 cp README.DATUMGRID "$TARGET" --profile projcdn --content-type "text/plain"
+aws s3 cp README.DATA "$TARGET" --profile projcdn --content-type "text/plain"
