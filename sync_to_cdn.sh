36ecf5c4ad34fc17c5d82fe07c57096210136474
Even Rouault <even.rouault@spatialys.com>
2020-02-03 00:02:25 +0100

    sync_to_cdn.sh: do not upload grid_tools/ directory

--- sync_to_cdn.sh
+++ sync_to_cdn.sh
@@ -3,8 +3,10 @@
 TARGET=s3://cdn.proj.org/
 
 for i in `ls -d *_*/`; do
-    echo "Synchronizing $i ..."
-    aws s3 sync $i "$TARGET" --exclude ".github/*" --profile projcdn
+    if test "$i" != "grid_tools/"; then
+        echo "Synchronizing $i ..."
+        aws s3 sync $i "$TARGET" --exclude ".github/*" --profile projcdn
+    fi
 done
 aws s3 cp geotiff.bundle.min.js "$TARGET" --profile projcdn
 aws s3 cp ol.js "$TARGET" --profile projcdn
