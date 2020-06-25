7b8f096248936e4327b7add592210f5711a3a820
Juan Hernando <juan.hernando@pix4d.com>
2020-06-25 09:27:46 +0200

    Added jp_gsi agency and GSIGEO2011 geoid grid for Japan mainland

---
+++ jp_gsi/GSIGEO2011.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+SCRIPT="
+import sys
+
+south, west, north, east = map(float, sys.argv[1:5])
+
+lines = sys.stdin.read().split('\n')
+
+metadata = lines[0].strip().split()[0:6]
+min_lat, min_lon, d_lat, d_lon = map(float, metadata[0:4])
+nrows, ncols = map(int, metadata[4:6])
+
+data = []
+row = []
+for line in lines[1:]:
+    line = line.strip()
+    if len(line) == 0:
+       continue
+    row += [v for v in map(float, line.split())]
+    if len(row) == ncols:
+        data.append(row)
+        row = []
+assert(len(data) == nrows)
+
+for i, row in enumerate(data):
+
+    lat = min_lat + d_lat * i
+
+    if lat + d_lat < south or lat - d_lat > north:
+        continue
+
+    for j, value in enumerate(row):
+        lon = min_lon + d_lon * j
+
+        if lon + d_lon < west or lon - d_lon > east:
+            continue
+
+        print('%.12f %.12f %.12f' % (lon, lat, value))
+"
+
+SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+if [ "$#" -lt 1 ]; then
+	echo "Usage: $(basename $0) source_file.txt"
+	exit 1
+fi
+
+SOURCE_FILE=$1
+
+RES_LAT=0.0166667
+RES_LONG=0.0250
+
+NO_DATA_VALUE="999.0000"
+
+function generate_grid
+{
+    TARGET_FILE=$1
+    SOUTH=$2
+    WEST=$3
+    NORTH=$4
+    EAST=$5
+
+    cat $SOURCE_FILE |
+    python -c "$SCRIPT" $SOUTH $WEST $NORTH $EAST > input
+
+    gdal_translate -mo "AREA_OR_POINT=Point" -r nearest -strict -a_nodata ${NO_DATA_VALUE} \
+                   -tr ${RES_LONG} ${RES_LAT} -a_srs EPSG:8994 input ${TARGET_FILE}
+
+    rm input
+}
+
+# Grid file for JGD2011 (vertical) height, Japan - onshore mainland, bounding box 30.94, 129.3, 45.54, 145.87
+# Area of use code 3263
+generate_grid jp_gsi_gsigeo2011_mainland.tif 30.94 129.3 45.54 145.87
+
+# Add metadata with grid_tools of PROJ-data repository
+$SCRIPT_PATH/../grid_tools/vertoffset_grid_to_gtiff.py \
+ --description "JDG2011 (EPSG:6667) to JGD2011 (vertical) height (EPSG:6695). Converted from gsigeo2011_ver2_1.asc" \
+ --type "GEOGRAPHIC_TO_VERTICAL" \
+ --copyright "Derived from work by the Geospatial Information Authority of Japan. CC-BY 4.0" \
+ --area-of-use "Japan - onshore mainland" \
+ --source-crs "EPSG:6667" \
+ --target-crs "EPSG:6695" \
+ jp_gsi_gsigeo2011_mainland.tif jp_gsi_gsigeo2011_mainland.tif
+
---
+++ jp_gsi/jp_gsi_README.txt
@@ -0,0 +1,19 @@
+# jp_gsi_README.txt
+
+The files in this section result from the conversion of datasets originating
+from [Geospatial Information Authority of Japan](http://www.gsi.go.jp/)
+
+## Included grids
+
+### JDG2011 (EPSG:6667) -> JDG2011 (vertical) height (EPSG:6695)
+
+*Source*: [Geospatial Information Authority of Japan](https://fgd.gsi.go.jp/download/geoid.php)
+*Format*: GeoTIFF converted by `GSIGGEO2011.sh`
+*License*: Derivative work based on gsigeo2011_ver2_1.asc (Geospatial Information Authority of Japan), CC-BY 4.0.
+*Horizontal CRS*: JDG2011 (EPSG:6667)
+
+Hybrid geoid created by fitting a high-resolution gravimetric geoid model for Japan, “JGEOID2008” to GNSS/leveling geoid undulations at 971 sites by the Least-Squares Collocation method. The standard deviation of the model at the GNSS/leveling sites is 1.8 cm. The geoid height shown in this model is the height over the GRS80-compliant ellipsoid using latitude and longitude values in the Japan Geodetic System 2011 frame.
+
+This grid file is a crop of the original bounding box to a the minimal pixel area that covers the area of use of EPSG:6695: Japan - onshore mainland (EPSG:3263 . The data for the Tokara, Amami, Okinawa, and Sakishima Islands in the Ryukyu Archipelago, the Daitö Islands and the Ogasawara Islands has been discarded due to the lack of a vertical coordinate reference system in the EPSG catalog.
+
+*  jp_gsi_gsigeo2011_mainland.tif
